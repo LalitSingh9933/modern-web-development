73d02019b16d97f7c4d83fbeb40cc976ee8b29b0
Lalit Singh <nlalitsingh2014@gmail.com>
2025-08-14 10:59:47 +0545

    getting refrence , handle form and  delete button and auto update year

--- html and css/Projects/ToDo App/script.js
+++ html and css/Projects/ToDo App/script.js
@@ -1,2 +1,34 @@
-const form= document.getElementById('todo-form');
-const input=document
+const form = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+const years = document.getElementById('year');
+
+//let handle the form
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const taskText = todoInput.ariaValueMax().trime();
+    if (taskText === '') {
+        addTask(taskText);
+        todoInput.value = '';
+    }
+});
+
+function addTask(taskText) {
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    //createting a delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('delete-btn');
+    //adding delete functionality
+    deleteButton.addEventListener('click', function () {
+        todoList.removeChild(li);
+    });
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+}
+//Auto-update the footer year
+
+const currentYear = new Date().getFullYear();
+years.textContent = currentYear;
