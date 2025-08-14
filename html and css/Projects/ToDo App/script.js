3936b65aa357efd9d97dfa804ecdc32ff6b8f1f5
Lalit Singh <nlalitsingh2014@gmail.com>
2025-08-14 14:06:45 +0545

    Adding localstorage and correct the styling

--- html and css/Projects/ToDo App/script.js
+++ html and css/Projects/ToDo App/script.js
@@ -3,6 +3,12 @@ const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 const years = document.getElementById('year');
 
+//using localstorage on page load
+window.addEventListener('DOMContentLoaded', () => {
+    const tasks = getTaskFromLocalStorage();
+    tasks.forEach(task=> addTask(task));
+});
+
 //let handle the form
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -10,10 +16,11 @@ form.addEventListener('submit', function (e) {
     const taskText = todoInput.value.trim();
     if (taskText !== '') {
         addTask(taskText);
+        saveTaskToLocalStorage(taskText);
         todoInput.value = '';
     }
 });
-
+//add task to local storage
 function addTask(taskText) {
     const li = document.createElement('li');
     li.textContent = taskText;
@@ -23,11 +30,34 @@ function addTask(taskText) {
     deleteButton.classList.add('delete-btn');
     //adding delete functionality
     deleteButton.addEventListener('click', function () {
-        todoList.removeChild(li);
+        li.remove();
+        removeTaskFromLocalStorage(taskText);
     });
     li.appendChild(deleteButton);
     todoList.appendChild(li);
 }
+
+//get task from local storage
+const getTaskFromLocalStorage= () =>{
+return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+
+//save a new task to localstorage
+const saveTaskToLocalStorage = (taskText)=> {
+    const tasks= getTaskFromLocalStorage();
+    tasks.push(taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+//remove task from local storage
+const removeTaskFromLocalStorage = (taskText) =>{
+    let tasks= getTaskFromLocalStorage();
+    tasks = tasks.filter(task=> task !== taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+
+
 //Auto-update the footer year
 
 const currentYear = new Date().getFullYear();
