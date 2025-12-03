0f45ad5e878082098654415256d8ff720f615b9a
Lalit Singh <nlalitsingh2014@gmail.com>
2025-12-03 16:26:51 +0545

    react and js

---
+++ javascript/Revise/External style/script.js
@@ -0,0 +1,6 @@
+console.log("Hello world ");
+const btn=document.getElementById("hello");
+btn.addEventListener('click',()=>{
+    alert("button is clicked by using external js style");
+    console.log("hello lalit your are doing good")
+});
